7d6473dd734afdecc12208b71bd335de6db6dd47
Milan Kubicek <milan.kubicek@reberinformatik.ch>
2016-02-23 14:41:05 +0100

    add initial code

---
+++ Code.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessSelfReference
+{
+    public class Code
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual IList<Code> RestrictedFor { get; set; }/* = new List<Code>();*/
+
+        public virtual IList<Code> Restricts { get; set; }/* = new List<Code>();*/
+    }
+}
---
+++ DBMetasdataSource.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+using Telerik.OpenAccess.Metadata.Fluent;
+
+namespace DataAccessSelfReference
+{
+    using Telerik.OpenAccess;
+    using Telerik.OpenAccess.Metadata;
+
+    public partial class DbMetadataSource : FluentMetadataSource
+    {
+        protected override IList<MappingConfiguration> PrepareMapping()
+        {
+            List<MappingConfiguration> mappingConfigurations = new List<MappingConfiguration>();
+
+            MappingConfiguration<Code> codeConfiguration = this.GetCodeMappingConfiguration();
+            mappingConfigurations.Add(codeConfiguration);
+
+            return mappingConfigurations;
+        }
+
+        public MappingConfiguration<Code> GetCodeMappingConfiguration()
+        {
+            MappingConfiguration<Code> configuration = this.GetCodeClassConfiguration();
+            this.PrepareCodePropertyConfigurations(configuration);
+            this.PrepareCodeAssociationConfigurations(configuration);
+
+            return configuration;
+        }
+
+        public MappingConfiguration<Code> GetCodeClassConfiguration()
+        {
+            MappingConfiguration<Code> configuration = new MappingConfiguration<Code>();
+            configuration.MapType().WithConcurencyControl(OptimisticConcurrencyControlStrategy.Changed).ToTable("Code");
+
+            return configuration;
+        }
+
+        public void PrepareCodePropertyConfigurations(MappingConfiguration<Code> configuration)
+        {
+            configuration.HasProperty(x => x.Id).IsIdentity(KeyGenerator.Default).WithDataAccessKind(DataAccessKind.ReadWrite).ToColumn("Id").IsNotNullable().HasColumnType("int").HasPrecision(0).HasScale(0);
+            configuration.HasProperty(x => x.Name).WithDataAccessKind(DataAccessKind.ReadWrite).ToColumn("Name").IsNotNullable().HasColumnType("nvarchar").HasLength(255);
+        }
+
+        public void PrepareCodeAssociationConfigurations(MappingConfiguration<Code> configuration)
+        {
+            configuration.HasAssociation(x => x.RestrictedFor).IsManaged().WithOpposite(x => x.Restricts).WithDataAccessKind(DataAccessKind.ReadWrite).MapJoinTable("CodeScope", (x, y) => new { Code_Id = x.Id, Scope_Id = y.Id }); 
+            //configuration.HasAssociation(x => x.Restricts).IsManaged().WithOpposite(x => x.RestrictedFor).WithDataAccessKind(DataAccessKind.ReadWrite).MapJoinTable("CodeScope", (x, y) => new { Scope_Id = x.Id, Code_Id = y.Id }); 
+        }
+    }
+}
